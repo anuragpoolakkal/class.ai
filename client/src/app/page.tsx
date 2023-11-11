af7980b5140a338519f448e72868233502241a84
Anurag <anuragrajanp@gmail.com>
2023-11-11 14:47:19 +0530

    client: landing page init

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+import { useRouter } from "next/navigation";
 export default function Home() {
-  return (
-    <main>
-     Class.AI
-    </main>
-  )
+	const router = useRouter();
+
+	return (
+		<main className="flex items-center w-[100vw] h-[100vh] overflow-hidden bg-gradient-to-tr from-[#dcd4ff] to-[#bdaeff]">
+			<div className="backdrop-filter backdrop-blur-lg">
+				<div className="flex">
+					<h1 className="ml-20 relative text-black text-9xl font-bold">Class.AI</h1>
+				</div>
+				<p className="text-black mt-5 text-3xl ml-20">AI-Powered Educational Content Generator</p>
+				<button className="btn btn-primary btn-lg ml-20 mt-10" onClick={() => router.push("/login")}>
+					Log In
+				</button>
+			</div>
+		</main>
+	);
 }
