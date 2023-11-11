f59fa28c4a1acab02fd8ef40a4068891fb0e3acc
Aqeel <aqeelten@gmail.com>
2023-11-11 15:31:30 +0530

    update

--- client/src/app/class/page.tsx
+++ client/src/app/class/page.tsx
@@ -1,8 +1,17 @@
 "use client";
-import { FiMic, FiRefreshCcw } from "react-icons/fi";
+import { useState } from "react";
+import { FiMic, FiMicOff, FiRefreshCcw } from "react-icons/fi";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
 
 export default function Class() {
-  
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
+  const [isMute, setIsMute] = useState<boolean>(false);
 
   return (
     <div className="flex flex-col items-center w-full h-[100vh] bg-white justify-evenly">
@@ -11,7 +20,7 @@ export default function Class() {
 
         </div>
         <div className="ml-5">
-          <h1 className="font-bold text-4xl mb-5">Sample Presentation</h1>
+          <h1 className="font-bold text-4xl mb-5">Sample Presentation</h1>  
           <p className="text-lg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, error. Tempore incidunt aut ea consectetur eius voluptatibus eum necessitatibus, recusandae, quae enim beatae veritatis sequi doloribus culpa ex unde voluptate!
           </p>
@@ -19,7 +28,9 @@ export default function Class() {
       </div>
       {/* Controls */}
       <div className="flex border-2 rounded-lg p-2">
-        <button className="btn btn-primary btn-square mr-2 text-xl"><FiMic /></button>
+        <button className="btn btn-primary btn-square mr-2 text-xl" onClick={()=>{
+          setIsMute(!isMute);
+        }}>{isMute ? <FiMicOff/> : <FiMic/>}</button>
         <button className="btn btn-primary btn-square text-xl" onClick={()=>window.location.reload()}><FiRefreshCcw /></button>
       </div>
     </div>
