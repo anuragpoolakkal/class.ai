d0ca04bffda986b7cbed07ecdcf612769b8c2b96
Aqeel <aqeelten@gmail.com>
2023-11-11 15:16:39 +0530

    update, react-icons

--- client/src/app/class/page.tsx
+++ client/src/app/class/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+import { FiMic, FiRefreshCcw } from "react-icons/fi";
+
 export default function Class() {
+  
+
   return (
-    <div>
-      <h1>class page</h1>
+    <div className="flex flex-col items-center w-full h-[100vh] bg-white justify-evenly">
+      <div className="flex w-[80vw] h-[80vh] p-5 bg-white rounded-xl shadow-xl border-2">
+        <div className="h-full w-full bg-black rounded-lg">
+
+        </div>
+        <div className="ml-5">
+          <h1 className="font-bold text-4xl mb-5">Sample Presentation</h1>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, error. Tempore incidunt aut ea consectetur eius voluptatibus eum necessitatibus, recusandae, quae enim beatae veritatis sequi doloribus culpa ex unde voluptate!
+          </p>
+        </div>
+      </div>
+      {/* Controls */}
+      <div className="flex border-2 rounded-lg p-2">
+        <button className="btn btn-primary btn-square mr-2 text-xl"><FiMic /></button>
+        <button className="btn btn-primary btn-square text-xl" onClick={()=>window.location.reload()}><FiRefreshCcw /></button>
+      </div>
     </div>
   )
 }
