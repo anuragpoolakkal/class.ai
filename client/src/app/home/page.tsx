748ffa5b8dc673da8374782504e0945f6cbc82c7
Anurag <anuragrajanp@gmail.com>
2023-11-11 19:07:15 +0530

    client: home page

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 import Navbar from "../components/Navbar";
+import { useRouter } from "next/navigation";
 import { CiCirclePlus } from "react-icons/ci";
 import { useState } from "react";
-import { FiFileText } from "react-icons/fi";
-import { bgColors } from "@/utils/utils";
+import { FiPlusCircle } from "react-icons/fi";
+import { bgColors, proficiencyLevels } from "@/utils/utils";
 
 export default function Home() {
 	const [classes, setClasses] = useState([]);
+	const [prompt, setPrompt] = useState("");
+	const [proLevel, setProLevel] = useState(0);
+
+	const router = useRouter();
+
 	return (
 		<>
 			<Navbar />
@@ -14,13 +20,18 @@ export default function Home() {
 				<div className="w-full h-full p-5 px-10">
 					<p className="text-2xl my-4 mb-7 font-semibold">My classes</p>
 					<div className="flex flex-wrap w-full">
-						<div className="flex flex-col items-center justify-center w-full h-full">
-							<CiCirclePlus className="h-40 w-40 mb-2" />
-							<p className="font-semibold text-xl">New Class</p>
+						<div
+							onClick={() => (document.getElementById("new_class_modal") as any).showModal()}
+							className="hover:shadow-2xl duration-100 cursor-pointer border-2 flex flex-col min-h-[400px] min-w-[350px] mb-10 mr-10 rounded-3xl shadow-lg overflow-hidden"
+						>
+							<div className="flex flex-col items-center justify-center w-full h-full">
+								<CiCirclePlus className="h-40 w-40 mb-2" />
+								<p className="font-semibold text-xl">New Form</p>
+							</div>
 						</div>
 					</div>
-					{classes?.map((class, index) => {
-						return (
+					{/* {classes?.map((class, index) => {
+						return 
 							<section
 								key={index}
 								translate="translateY(10px)"
@@ -51,10 +62,50 @@ export default function Home() {
 									</div>
 								</div>
 							</section>
-						);
-					})}
+						
+					})} */}
 				</div>
 			</main>
+			{/* Modals */}
+			{/* New Class modal */}
+			<dialog id="new_class_modal" className="modal">
+				<div className="modal-box max-w-xl">
+					<h3 className="flex items-center font-bold text-2xl">
+						<FiPlusCircle className="mr-2" /> Create class
+					</h3>
+					<textarea value={prompt}
+						onChange={(x) => setPrompt(x.target.value)}
+						placeholder="Tell us about your class..."
+						className="my-5 textarea textarea-bordered textarea-lg min-h-[250px] w-full max-w-3xl"
+					></textarea>
+					<h3 className="my-5 font-bold">Proficiency level</h3>
+					<div className="row" style={{ marginBottom: "50px" }}>
+						<input
+							type="range"
+							value={proLevel}
+							min={0}
+							max="4"
+							className="range"
+							step="0"
+							onChange={(e) => setProLevel(parseInt(e.target.value))}
+						/>
+						<div className="w-full flex justify-between text-xs px-2">
+							<span>{proficiencyLevels[0]}</span>
+							<span>|</span>
+							<span>|</span>
+							<span>|</span>
+							<span>{proficiencyLevels[4]}</span>
+						</div>
+					</div>
+					<button className="btn w-full btn-primary" onClick={() => router.push("/class/[classId]")}>
+						Create
+					</button>
+				</div>
+				<form method="dialog" className="modal-backdrop">
+					<button>close</button>
+				</form>
+			</dialog>
+			{/* New Class modal end */}
 		</>
 	);
 }
--- client/src/utils/utils.ts
+++ client/src/utils/utils.ts
@@ -29,4 +29,15 @@ const bgColors = {
 	z: ["#f12711", "#f5af19"],
 };
 
-export { serverURL, bgColors };
+const proficiencyLevels = ["Noob", "Beginner", "Intermediate", "Advanced", "Expert", "Pro"];
+
+const proficiencyPrompt = {
+	0: "5 year old",
+	1: "beginner",
+	2: "high school student",
+	3: "college student",
+	4: "graduate student",
+	5: "expert",
+};
+
+export { serverURL, bgColors, proficiencyLevels, proficiencyPrompt };
